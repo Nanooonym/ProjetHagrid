ed70413d9e604d3c7993c5bc728ed74db6ae05f5
Emeline FE <emeline.feron@laposte.net>
2020-03-24 14:20:01 +0100

    Categorie update

--- AppliTrocEnchere/src/fr/eni/appliTrocEnchere/bo/Categorie.java
+++ AppliTrocEnchere/src/fr/eni/appliTrocEnchere/bo/Categorie.java
@@ -1,5 +1,9 @@
 package fr.eni.appliTrocEnchere.bo;
 
+import java.util.ArrayList;
+
+
+
 /**
  * 
  * @author Emeline
@@ -10,6 +14,9 @@ public class Categorie {
 
 	private int noCategorie;
 	private String libelle;
+	//Mise en commentaire de l'arrayList arcticleVendu en attendant la bo articleVendu
+	//private ArrayList<ArticleVendu> categorieArticle = null;
+	
 	
 	//Creation du constructeur par défaut
 	public Categorie() {
@@ -50,6 +57,38 @@ public class Categorie {
 		builder.append("]");
 		return builder.toString();
 	}
+
+	//Creation du HashCode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((libelle == null) ? 0 : libelle.hashCode());
+		result = prime * result + noCategorie;
+		return result;
+	}
+	
+	//Creation du Equals
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categorie other = (Categorie) obj;
+		if (libelle == null) {
+			if (other.libelle != null)
+				return false;
+		} else if (!libelle.equals(other.libelle))
+			return false;
+		if (noCategorie != other.noCategorie)
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 }
