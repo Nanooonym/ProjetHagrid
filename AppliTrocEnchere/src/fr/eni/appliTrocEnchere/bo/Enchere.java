6945bca4a1affa2ce64c5976eee2790c488ef9aa
Emeline FE <emeline.feron@laposte.net>
2020-03-25 12:22:59 +0100

    enchere update

--- AppliTrocEnchere/src/fr/eni/appliTrocEnchere/bo/Enchere.java
+++ AppliTrocEnchere/src/fr/eni/appliTrocEnchere/bo/Enchere.java
@@ -8,15 +8,16 @@ public class Enchere {
 	private int noArticle;
 	private LocalDate dateEnchere;
 	private int montantEnchere;
+	private Utilisateur utilisateur; 
+	private ArticleVendu articleVendu;
 	
-	//Constructeur par défaut
+	//Constructeur par defaut
 	
 	public Enchere() {
-		
+		utilisateur = new Utilisateur();
+		articleVendu = new ArticleVendu();
 	}
-	
-	//Getters & Setters
-	
+
 	public int getNoUtilisateur() {
 		return noUtilisateur;
 	}
@@ -49,37 +50,21 @@ public class Enchere {
 		this.montantEnchere = montantEnchere;
 	}
 
-	
-	
-	//Equals avec noArticle & nUtilisateur
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + noArticle;
-		result = prime * result + noUtilisateur;
-		return result;
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Enchere other = (Enchere) obj;
-		if (noArticle != other.noArticle)
-			return false;
-		if (noUtilisateur != other.noUtilisateur)
-			return false;
-		return true;
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
 	}
-	
-	
-	//toString avec tous les attributs
+
+	public ArticleVendu getArticleVendu() {
+		return articleVendu;
+	}
+
+	public void setArticleVendu(ArticleVendu articleVendu) {
+		this.articleVendu = articleVendu;
+	}
 
 	@Override
 	public String toString() {
@@ -92,11 +77,46 @@ public class Enchere {
 		builder.append(dateEnchere);
 		builder.append(", montantEnchere=");
 		builder.append(montantEnchere);
+		builder.append(", utilisateur=");
+		builder.append(utilisateur);
+		builder.append(", articleVendu=");
+		builder.append(articleVendu);
 		builder.append("]");
 		return builder.toString();
 	}
-	
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((articleVendu == null) ? 0 : articleVendu.hashCode());
+		result = prime * result + ((utilisateur == null) ? 0 : utilisateur.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enchere other = (Enchere) obj;
+		if (articleVendu == null) {
+			if (other.articleVendu != null)
+				return false;
+		} else if (!articleVendu.equals(other.articleVendu))
+			return false;
+		if (utilisateur == null) {
+			if (other.utilisateur != null)
+				return false;
+		} else if (!utilisateur.equals(other.utilisateur))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 	
