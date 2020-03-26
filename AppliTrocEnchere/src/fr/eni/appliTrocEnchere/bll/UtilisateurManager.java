f5983986bb7241b9725abf9aea1db15055d7e829
Maxime <maxime.duong2020@campus-eni.fr>
2020-03-26 10:46:58 +0100

    Merge branch 'UtilisateurDAOJdbcImpl'
    Conflicts:
    	AppliTrocEnchere/src/fr/eni/appliTrocEnchere/bll/UtilisateurManager.java
    	AppliTrocEnchere/src/fr/eni/appliTrocEnchere/dal/CodesResultatDAL.java
    	AppliTrocEnchere/src/fr/eni/appliTrocEnchere/dal/UtilisateurDAO.java
    	AppliTrocEnchere/src/fr/eni/appliTrocEnchere/dal/jdbc/UtilisateurDAOJdbcImpl.java
    	AppliTrocEnchere/src/fr/eni/appliTrocEnchere/ihm/ServletTest.java

--- AppliTrocEnchere/src/fr/eni/appliTrocEnchere/bll/UtilisateurManager.java
+++ AppliTrocEnchere/src/fr/eni/appliTrocEnchere/bll/UtilisateurManager.java
@@ -18,6 +18,12 @@ public class UtilisateurManager {
 
 	}
 
+	public Utilisateur selectUtilisateurById(int noUtilisateur) throws BusinessException{
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur = utilisateurDAO.selectUtilisateurById(noUtilisateur);
+		return utilisateur;
+	}
+	
 	public List<Utilisateur> selectUtilisateurs () throws BusinessException{
 		List<Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
 		utilisateurs = utilisateurDAO.selectUtilisateurs();
